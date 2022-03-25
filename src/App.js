e487e9df9cb6ebab43fcb05cb5094b57d665dc83
Bharat Pratap <bp171020@gmail.com>
2022-03-25 23:38:00 +0530

    header basics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Header from './components/header/header';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Header />
+
+      <div className="App">
       Minor Project
-    </div>
+      </div>
+    
+    </>
+    
   );
 }
 
---
+++ src/components/header/header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './header.css'
+
+const Header = ()=> {
+    return (
+        <>
+        <div className='header' >
+            <h1 className='heading-main inline'>Sumry</h1>
+            <h2 className='inline' >LogIn</h2>
+            <h2 className='inline' >SignUp</h2>
+        </div>
+            
+        </>
+    )
+}
+
+export default Header;
