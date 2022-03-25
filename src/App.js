584a3ce300c237a39a8f72909843b1506dcc6260
Nikhil Ranjan Kumar <nrk628@gmail.com>
2022-03-25 23:48:39 +0530

    signin page added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import SignIn from './components/SignIn/SignIn';
 function App() {
   return (
     <div className="App">
-      Minor Project
+      <SignIn />
     </div>
   );
 }
